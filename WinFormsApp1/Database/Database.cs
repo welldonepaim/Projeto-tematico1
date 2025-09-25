f58dc8bdeceb8e0832c0b7e86b3f898371b3a521
pedrocurzel <67026810+pedrocurzel@users.noreply.github.com>
2025-09-25 20:53:24 -0300

    criacao db

---
+++ WinFormsApp1/Database/Database.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp1.Models;
+
+namespace WinFormsApp1;
+public class Database : DbContext
+{
+    //public Database(DbContextOptions opts) : base(opts) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=myapp.db");
+
+    public DbSet<UserModel> users { get; set; }
+}
